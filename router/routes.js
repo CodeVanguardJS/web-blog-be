3d118d69065c781b7edb7cb692211e1eccf70296
arief2020 <msyaifullahalarief@gmail.com>
2025-01-15 21:42:16 +0700

    feat: add new example api (108)

--- router/routes.js
+++ router/routes.js
@@ -8,7 +8,7 @@ router.get('/api/hello', function (req, res) {
 })
 
 router.get('/api/health', function (req, res) {
-  res.status(200).json({ message: 'success hit api server' })
+  res.status(200).json({ message: 'successfully hit api server' })
 })
 
 router.use('/auth', authRoute)
