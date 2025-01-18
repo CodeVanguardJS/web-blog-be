590a5f58e0b2be37e15a03761a72539062a3a414
arief2020 <msyaifullahalarief@gmail.com>
2025-01-18 09:54:02 +0700

    fix: add status code in success response function (106)

--- helpers/response.js
+++ helpers/response.js
@@ -1,5 +1,5 @@
-function successResponse (res, data, message = 'Success') {
-  return res.status(200).json({
+function successResponse (res, data, message = 'Success', statusCode = 200) {
+  return res.status(statusCode).json({
     status: true, // success or error
     data, // any, maybe object or array
     message, // any
