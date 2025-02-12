3216b3de4000a2148ed5ad68dd82f8d161f6d11e
arief2020 <msyaifullahalarief@gmail.com>
2025-02-12 20:51:00 +0700

    feat: add endpoint article (107)

---
+++ controllers/article.controller.js
@@ -0,0 +1,91 @@
+const { successResponse } = require('../helpers/response')
+const cloudinaryUpload = require('../libs/cloudinary')
+const ArticleService = require('../services/article.service')
+
+class ArticleController {
+  static async getAll (req, res, next) {
+    try {
+      const category = await ArticleService.getAll(req.query)
+      return successResponse(res, category, 'Success Get All Article')
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async getById (req, res, next) {
+    try {
+      const { id } = req.params
+      const article = await ArticleService.getById(id)
+      return successResponse(res, article, 'Success Get Category By Id')
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static create = async (req, res, next) => {
+    try {
+      const { title, categoryId, description, recipes } = req.body
+      const photo = req.file.path
+      if (!title || !categoryId || !description || !recipes || !photo) {
+        const error = new Error('field  is required')
+        error.name = 'BadRequest'
+        throw error
+      }
+      const body = { title, categoryId, description, recipes }
+      const article = await ArticleService.create(body, photo)
+
+      return successResponse(res, article, 'Success Post Article', 201)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async update (req, res, next) {
+    try {
+      const { title, categoryId, description } = req.body
+      const { id } = req.params
+      const photo = req.file.path
+
+      if (!title || !categoryId || !description || !photo) {
+        const error = new Error('field  is required')
+        error.name = 'BadRequest'
+        throw error
+      }
+
+      const body = { title, category_id: +categoryId, description }
+      const article = await ArticleService.update(id, body, photo)
+      return successResponse(res, article, 'Success Put Article')
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async delete (req, res, next) {
+    try {
+      const { id } = req.params
+      const article = await ArticleService.delete(id)
+      return successResponse(res, article, 'Success Delete Article')
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async upload (req, res) {
+    try {
+      const result = await cloudinaryUpload(req.file.path)
+      res.status(200).json({
+        success: true,
+        message: 'Uploaded!',
+        url: result.secure_url
+      })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({
+        success: false,
+        message: 'Error'
+      })
+    }
+  }
+}
+
+module.exports = ArticleController
---
+++ repositories/article.repository.js
@@ -0,0 +1,107 @@
+const prisma = require('../libs/prisma')
+
+class ArticleRepository {
+  static async getAll (filter) {
+    try {
+      const article = await prisma.article.findMany({
+        ...filter,
+        include: {
+          bookmark: true,
+          category: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              photo_url: true,
+              email: true
+            }
+          }
+        }
+      })
+      return article
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static async getTotalArticle (filter) {
+    try {
+      const article = await prisma.article.count({
+        ...filter
+      })
+      return article
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static async getById (id) {
+    try {
+      const article = await prisma.article.findUnique({
+        where: { id },
+        include: {
+          recipe: true,
+          like: true,
+          bookmark: true,
+          category: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              photo_url: true,
+              email: true
+            }
+          }
+        }
+      })
+      return article
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static create = async (params) => {
+    try {
+      const category = await prisma.article.create({ data: params, include: { recipe: true } })
+      return category
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static update = async (id, params) => {
+    try {
+      const data = {
+        title: params.title,
+        description: params.description,
+        photo_url: params.photo_url,
+        category: {
+          connect: {
+            id: params.category_id
+          }
+        }
+      }
+      const category = await prisma.article.update({ where: { id }, data })
+      return category
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  static delete = async (id) => {
+    try {
+      const category = await prisma.article.delete({ where: { id } })
+      return category
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+}
+
+module.exports = ArticleRepository
