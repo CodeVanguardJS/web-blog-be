e8c517b57f054004b0e8ae7d79418b9580f00e2c
arief2020 <msyaifullahalarief@gmail.com>
2025-02-12 20:48:23 +0700

    fix: fixing response delete category for not found category

--- router/routes.js
+++ router/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const authRoute = require('./rest/auth.route')
 const categoryRoute = require('./rest/category.route')
+const articleRoute = require('./rest/article.route')
+const recipeRoute = require('./rest/recipe.route')
 
 router.get('/api/hello', function (req, res) {
   res.status(200).json({ message: 'hello world' })
@@ -13,5 +15,7 @@ router.get('/api/health', function (req, res) {
 
 router.use('/auth', authRoute)
 router.use('/categories', categoryRoute)
+router.use('/articles', articleRoute)
+router.use('/recipes', recipeRoute)
 
 module.exports = router
