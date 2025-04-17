05d45c72ad6723d28f817013ec82e1a8bb0931cb
Kungpao-pixel <azharretro46@gmail.com>
2025-04-17 14:08:42 +0700

    chore: commit changed

--- services/like.service.js
+++ services/like.service.js
@@ -1,18 +1,18 @@
 /* eslint-disable semi */
-const LikeRepository = require('../repositories/like.repository');
+const LikeRepository = require('../repositories/like.repository')
 
 class LikeService {
   static async toggleLike (userId, articleId) {
-    return await LikeRepository.toggleLike(userId, articleId);
+    return await LikeRepository.toggleLike(userId, articleId)
   }
 
   static async getUserLikes (userId) {
-    return await LikeRepository.getUserLikes(userId);
+    return await LikeRepository.getUserLikes(userId)
   }
 
   static async getLikeCount (articleId) {
-    return await LikeRepository.getLikeCount(articleId);
+    return await LikeRepository.getLikeCount(articleId)
   }
 }
 
-module.exports = LikeService;
+module.exports = LikeService
