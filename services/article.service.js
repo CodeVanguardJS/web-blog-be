b3589d1e7e2ab0971aaa85e4fe0e82b950a6d779
arief2020 <msyaifullahalarief@gmail.com>
2025-05-05 21:59:54 +0700

    fix: :bug: fixing bug when upload only one recipes

--- services/article.service.js
+++ services/article.service.js
@@ -187,10 +187,16 @@ class ArticleService {
 
       const recipeData = []
 
-      for (const element of recipes) {
+      if (typeof recipes === 'string') {
         recipeData.push({
-          content: element
+          content: recipes
         })
+      } else {
+        for (const element of recipes) {
+          recipeData.push({
+            content: element
+          })
+        }
       }
 
       const data = {
@@ -247,8 +253,6 @@ class ArticleService {
         const photoUpload = await cloudinaryUpload(photo)
         data.photo_url = photoUpload.secure_url
       }
-      // const photoUpload = await cloudinaryUpload(photo)
-      // data.photo_url = photoUpload.secure_url
       const recipe = await ArticleRepository.update(+id, data)
       return recipe
     } catch (error) {
