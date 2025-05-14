d863eb232fff91aea958454fc562a9e3d35c0c49
Kungpao-pixel <azharretro46@gmail.com>
2025-05-14 14:54:07 +0700

    fix: Fixing response, changed file name, password in response, and testing

--- router/rest/test/auth.test.js
+++ router/rest/test/auth.test.js
@@ -38,7 +38,9 @@ describe('Auth Routes', () => {
   })
 
   test('Get profile (auth/me)', async () => {
-    const res = await request(app).get(`${BASE_URL}/me`).set('Authorization', `Bearer ${token}`)
+    const res = await request(app)
+      .get(`${BASE_URL}/me`)
+      .set('Authorization', `Bearer ${token}`)
 
     expect(res.statusCode).toBe(200)
     expect(res.body.status).toBe(true)
