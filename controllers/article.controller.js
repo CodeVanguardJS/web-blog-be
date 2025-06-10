422d4830956f67fee6801a87b452e39e32a51f90
Muhammad Syaifullah Al Arief <68526285+arief2020@users.noreply.github.com>
2025-06-10 16:30:33 +0700

    Fix/109 endpont article (#14)
    * fix: add endpoint get article by category
    
    * fix: :bug: add filter draft in get all article endpoint (109)
    
    * fix: :bug: add middleware for post put delete article (109)
    
    * style: rename variable categoryResp to articleResp
    
    * fix: fixing bug delete article and recipe
    
    * fix: :bug: make upload photo is optional for endpoint update article
    
    * fix: :bug: fixing bug when upload only one recipes
    
    * fix: fixing response message for create many recipe and Seperate publish and draft articles when create article
    
    * fix: add endpoint get all by me
    
    * fix: add articleType parameter on put article endpoint
    
    * fix: add articleType parameter on put article endpoint

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -12,6 +12,17 @@ class ArticleController {
     }
   }
 
+  static async getByUser (req, res, next) {
+    try {
+      const { id } = req.user
+      console.log(`id: ${id}`)
+      const category = await ArticleService.getAll(req.query, id)
+      return successResponse(res, category, 'Success Get Category By Id')
+    } catch (error) {
+      next(error)
+    }
+  }
+
   static async getByCategory (req, res, next) {
     try {
       const { id } = req.params
@@ -53,7 +64,7 @@ class ArticleController {
   static async update (req, res, next) {
     try {
       let photo = null
-      const { title, categoryId, description } = req.body
+      const { title, categoryId, description, articleType } = req.body
       const { id } = req.params
 
       if (req.file) {
@@ -66,7 +77,7 @@ class ArticleController {
         throw error
       }
 
-      const body = { title, category_id: +categoryId, description }
+      const body = { title, category_id: +categoryId, description, type: articleType }
       const article = await ArticleService.update(id, body, photo, req.user.id)
       return successResponse(res, article, 'Success Put Article')
     } catch (error) {
